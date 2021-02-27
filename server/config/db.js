14dc920f3e74d863b410331025ff337a95fc08b6
Janesha Sharma <janeshasharma09@gmail.com>
2021-02-27 01:03:17 -0500

    first commit

---
+++ server/config/db.js
@@ -0,0 +1,6 @@
+module.exports = 
+{
+    //"URI": "mongodb://localhost/book_store"
+    "URI": "mongodb+srv://janesha:J@nesha09@cluster0.kfpho.mongodb.net/book-store?retryWrites=true&w=majority",
+    "Secret": 'SomeSecret'
+}
